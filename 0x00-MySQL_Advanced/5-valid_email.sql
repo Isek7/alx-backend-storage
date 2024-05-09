35cd657f4dca32ac5dc49a21852ebfbeb791e5fb
Isek7 <bengabrielisek@gmail.com>
2024-05-09 01:34:24 +0100

    All Set

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- Creates a trigger that resets the attribute valid_email
 -- only when the email has been changed.
+-- @author Ben Gabriel Isek <https://github.com/Isek7>
 DROP TRIGGER IF EXISTS validate_email;
 DELIMITER $$
 CREATE TRIGGER validate_email
